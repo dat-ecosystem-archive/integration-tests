c16145fb7f634f43187d5fde3a874977e14dc3d1
Mathias Buus <mathiasbuus@gmail.com>
2018-01-19 21:32:35 +0100

    add basic mdns test

--- tests/multicast-discovery/discover.js
+++ tests/multicast-discovery/discover.js
@@ -2,6 +2,47 @@ var test = require('tapenet')
 
 var {h1, h2} = test.topologies.basic()
 
+test('base mdns', function (t) {
+  t.plan(3 + 1 + 3 + 1)
+
+  t.run(h1, () => {
+    var dns = require('multicast-dns')()
+
+    dns.once('query', function (query) {
+      t.same(query.questions.length, 1)
+      t.same(query.questions[0].type, 'A')
+      t.same(query.questions[0].name, 'hello.local')
+      dns.response({
+        answers: [{
+          type: 'A',
+          name: 'hello.local',
+          data: '127.0.0.1'
+        }]
+      }, err => t.error(err, 'no response error'))
+    })
+
+    h1.emit('ready')
+  })
+
+  t.run(h2, () => {
+    var dns = require('multicast-dns')()
+
+    h1.on('ready', function () {
+      dns.once('response', function (response) {
+        t.same(response.answers[0].type, 'A')
+        t.same(response.answers[0].name, 'hello.local')
+        t.same(response.answers[0].data, '127.0.0.1')
+      })
+      dns.query({
+        questions: [{
+          type: 'A',
+          name: 'hello.local'
+        }]
+      }, err => t.error(err, 'no query error'))
+    })
+  })
+})
+
 test('listening node (h1) discovers announcing node (h2)', function (t) {
   t.run(h1, () => {
     var discovery = require('dns-discovery')
